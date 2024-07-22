4721f4866361b33541c3917117cb97fa4e11cc5d
Paulo Douglas <paulodouglasmdias>
2024-07-22 10:15:18 -0300

    Adicionando a confirmação para a entrada das funções

--- Clientes/ClientesController.py
+++ Clientes/ClientesController.py
@@ -7,7 +7,7 @@ import libs.utils as ut
 def register_client():
     clv.cadastrar_clientes()
     while True:
-        conf = clv.confirmacao('um cadastro')
+        conf = ut.confirmacao('cliente','um cadastro')
         match conf:
             case '1':
                 nome = ins.insert_name()
@@ -15,7 +15,6 @@ def register_client():
                 endereco = ins.insert_address()
                 clientes = clm.clientes
                 clientes[cpf] = [nome, endereco]
-                clm.salvar_clientes(clientes)
                 print(f'Nome - {nome} | CPF - {cpf} | Endereço - {endereco}')
                 print()
                 ut.mostrar_mensagem('Cliente cadastrado com sucesso!')
@@ -28,34 +27,62 @@ def register_client():
 
 def dados_exibir():
     clv.exibir_dados1()
-    cpf = gt.get_cpf()
-    clientes = clm.clientes
-    if cpf in clientes:
-        nome, endereco = clm.chamar_dados(cpf, clientes)
-        clv.dados_clientes(cpf, nome, endereco)
-    else:
-        ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+    while True:
+        conf = ut.confirmacao('cliente', 'a exibição dos seu dados')
+        match conf:
+            case '1':
+                cpf = gt.get_cpf()
+                clientes = clm.clientes
+                if cpf in clientes:
+                    nome, endereco = clm.chamar_dados(cpf, clientes)
+                    clv.dados_clientes(cpf, nome, endereco)
+                else:
+                    ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+                    break
+            case '0':
+                break
+            case _:
+                ut.mensagem_erro('Caro usuário, a opção informada não é válida. Por favor, digite 1 para continuar com o cadastro e 0 para sair.')
+                continue
 
 def dados_alterar():
     clv.alterar_dados()
-    cpf = gt.get_cpf()
-    clientes = clm.clientes
-    if cpf in clientes:
-        nome, endereco = clm.chamar_dados(cpf, clientes)
-        clv.dados_clientes(cpf, nome, endereco)
-        clm.alt_decisao(cpf, clientes)
-        clm.salvar_clientes(clientes)
-    else:
-        ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+    while True:
+        conf = ut.confirmacao('cliente', 'a alteração dos seus dados')
+        match conf:
+            case '1':
+                cpf = gt.get_cpf()
+                clientes = clm.clientes
+                if cpf in clientes:
+                    nome, endereco = clm.chamar_dados(cpf, clientes)
+                    clv.alterar_dados2(cpf, nome, endereco)
+                    clm.alt_decisao(cpf, clientes)
+                else:
+                    ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+                    break
+            case '0':
+                break
+            case _:
+                ut.mensagem_erro('Caro usuário, a opção informada não é válida. Por favor, digite 1 para continuar com o cadastro e 0 para sair.')
+                continue
 
 def cliente_excluir():
     clv.excluir_cliente()
-    cpf = gt.get_cpf()
-    clientes = clm.clientes
-    if cpf in clientes:
-        nome, endereco = clm.chamar_dados(cpf, clientes)
-        clv.dados_clientes(cpf, nome, endereco)
-        clm.del_cliente(cpf, clientes)
-        clm.salvar_clientes(clientes)
-    else:
-        ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+    while True:
+        conf = ut.confirmacao('cliente', 'deletar sua conta')
+        match conf:
+            case '1':
+                cpf = gt.get_cpf()
+                clientes = clm.clientes
+                if cpf in clientes:
+                    nome, endereco = clm.chamar_dados(cpf, clientes)
+                    clv.dados_clientes(cpf, nome, endereco)
+                    clm.del_cliente(cpf, clientes)
+                else:
+                    ut.mostrar_mensagem('O CPF informado não está cadastrado no nosso sistema.')
+                    break
+            case '0':
+                break
+            case _:
+                ut.mensagem_erro('Caro usuário, a opção informada não é válida. Por favor, digite 1 para continuar com o cadastro e 0 para sair.')
+                continue
