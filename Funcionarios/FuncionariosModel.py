e521b4036e2389ac38fe8b4881d48e1b628d063d
Paulo Douglas <paulodouglasmdias>
2024-07-23 10:25:20 -0300

    Funções de gerenciamento de cardápio

--- Funcionarios/FuncionariosModel.py
+++ Funcionarios/FuncionariosModel.py
@@ -1,5 +1,7 @@
+import Clientes.ClientesModel as clm
 import libs.insertes as ins
 import libs.utils as ut
+import libs.get as gt
 import textwrap
 import pickle
 
@@ -20,47 +22,49 @@ def salvar_cardapio():
     arq_cardapio.close()
 
 def formatar_dados(cardapio):
-    dados_formatados = []
-    for id, pizza in cardapio.items():
-        nome = pizza[0]
-        ingredientes = ' - '.join(pizza[1])
-        valores = ' '.join([f'R$ {float(valor):.2f}' for valor in pizza[2]])
+        dados_formatados = []
+        for id, pizza in cardapio.items():
+            if pizza[3] == False:
+                continue  # Ignora pizzas com estado False
 
-        nome_quebrado = textwrap.fill(nome, width=18)
-        ingredientes_quebrados = textwrap.fill(ingredientes, width=69)
-        valores_quebrados = textwrap.fill(valores, width=31)
+            nome = pizza[0]
+            ingredientes = ' - '.join(pizza[1])
+            valores = ' '.join([f'R$ {float(valor):.2f}' for valor in pizza[2]])
 
-        linhas_nome = nome_quebrado.split('\n')
-        linhas_ing = ingredientes_quebrados.split('\n')
-        linhas_valores = valores_quebrados.split('\n')
+            nome_quebrado = textwrap.fill(nome, width=18)
+            ingredientes_quebrados = textwrap.fill(ingredientes, width=69)
+            valores_quebrados = textwrap.fill(valores, width=31)
 
-        max_linhas = max(len(linhas_nome), len(linhas_ing), len(linhas_valores))
-        for i in range(max_linhas):
-            linha = '|{:^8}'.format(id if i == 0 else '')
-            nome_final = '{:^18}'.format(linhas_nome[i]) if i < len(linhas_nome) else '{:^18}'.format('')
-            ing_final = '{:^69}'.format(linhas_ing[i]) if i < len(linhas_ing) else '{:^69}'.format('')
-            valores_final = '{:^31}'.format(linhas_valores[i]) if i < len(linhas_valores) else '{:^31}'.format('')
-            linha += f'|{nome_final}|{ing_final}|{valores_final} |'
-            dados_formatados.append(linha)
-        dados_formatados.append('|--------|------------------|---------------------------------------------------------------------|--------------------------------|')  # Linha separadora
-    return dados_formatados
+            linhas_nome = nome_quebrado.split('\n')
+            linhas_ing = ingredientes_quebrados.split('\n')
+            linhas_valores = valores_quebrados.split('\n')
 
-def solicitar_id():
+            max_linhas = max(len(linhas_nome), len(linhas_ing), len(linhas_valores))
+            for i in range(max_linhas):
+                linha = '|{:^8}'.format(id if i == 0 else '')
+                nome_final = '{:^18}'.format(linhas_nome[i]) if i < len(linhas_nome) else '{:^18}'.format('')
+                ing_final = '{:^69}'.format(linhas_ing[i]) if i < len(linhas_ing) else '{:^69}'.format('')
+                valores_final = '{:^31}'.format(linhas_valores[i]) if i < len(linhas_valores) else '{:^31}'.format('')
+                linha += f'|{nome_final}|{ing_final}|{valores_final} |'
+                dados_formatados.append(linha)
+            dados_formatados.append('|--------|------------------|---------------------------------------------------------------------|--------------------------------|')  # Linha separadora
+        return dados_formatados
+
+def solicitar_pizza(cardapio):
     while True:
-        id = input('Informe o ID da pizza que você deseja alterar os dados. Tecle 0 para sair: ')
-        if id == '0':
-            return
-        try:
-            id = int(id)
+        id = gt.get_pizza()
+        id = int(id)
+        if id in cardapio:
             return id
-        except ValueError:
-            ut.mensagem_erro('ID inválido. Por favor, insira um número.')
+        else:
+            ut.mensagem_erro('Pizza não encontrada.')
             continue
 
 def editar_pizza(cardapio):
     resposta = ['s', 'sim', 'n', 'nao', 'não']
-    id = solicitar_id()
-    if id in cardapio:
+    id = solicitar_pizza(cardapio)
+    
+    if id in cardapio and cardapio[id][3] == True:
         while True:
             decisao = input('Qual dado da pizza você deseja alterar? (1: Nome, 2: Ingredientes, 3: Valores): ')
             match decisao:
@@ -86,9 +90,26 @@ def editar_pizza(cardapio):
                     if resp in ['n', 'nao', 'não']:
                         ut.mostrar_mensagem('Alterações feitas com sucesso!!')
                         return
+                    elif resp in ['s', 'sim']:
+                        break
                 else:
                     ut.mensagem_erro('Resposta inválida. Responda com somente SIM ou NÃO.')
     else:
-        ut.mostrar_mensagem('ID da pizza inválido. Por favor, informe um ID válido.')
+        ut.mostrar_mensagem('ID da pizza inválido ou pizza inativa. Por favor, informe um ID válido de uma pizza ativa.')
+
+def del_pizza():
+    while True:
+        conf = ut.confirmacao('funcionário', 'a exclusão de uma pizza')
+        match conf:
+            case '1':
+                id = solicitar_pizza(cardapio)
+                cardapio[id][3] = False
+                ut.mostrar_mensagem('Exclusão bem-sucedida. Até mais.')
+                break
+            case '0':
+                ut.mostrar_mensagem('Operação de exclusão cancelada.')
+                break
+            case _:
+                ut.mensagem_erro('Resposta informada não é válida. Escolha entre: 1 (continuar com a exclusão) e 2 (sair).')
 
 carregar_cardapio()
